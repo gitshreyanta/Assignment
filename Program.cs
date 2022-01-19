5d8c42b8602794bf282c2f010d6459208ea621c3
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-19 19:41:41 +0530

    [Shreya] initial commit

--- Program.cs
+++ Program.cs
@@ -7,17 +7,6 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //FlipCoin.FindFLipCoinPercentage();
-            //LeapYear.LeapYearProgram();
-            //Poweroftwo.PowerofTwoProgram();
-            //Harmonic.HarmonicFunction();
-            //FactorialExample factorial = new FactorialExample();
-            //factorial.Main();
-            //QuotAndRem.QuotientAndRemainder();
-            //EvenAndOdd.EvenAndOddProgram();
-            //Alphabet.VowelsAndConsonant();
-            //Largest.LargestNumbers();
-            
         }
     }
 }
